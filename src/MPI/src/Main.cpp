adb4ecbbea5c30e1d13afafd90f5273d2b73bfa4
K <Akarfire@gmail.com>
2026-03-13 15:57:16 +0300

    [DEV] MPI setup

--- src/MPI/src/Main.cpp
+++ src/MPI/src/Main.cpp
@@ -1,3 +1,5 @@
+#include <mpi.h>
+
 #include "Constants.h"
 #include "FieldSolver.h"
 #include "Grid.h"
@@ -6,8 +8,8 @@
 #include "FieldGeneratorFdtd.h"
 #include "Vectors.h"
 
+using GridType = pfc::YeeGrid;
 namespace pfc {
-using GridType = YeeGrid;
 
  FP3 eFunc(FP3 coords, FP t) 
  {
@@ -25,7 +27,7 @@ FP3 bFunc(FP3 coords, FP t)
     return e;
 }
 
-void initializeGrid(std::unique_ptr<GridType>& grid) 
+void initializeGrid(std::unique_ptr<GridType>& grid, const FP3& coordOffset) 
 {
     Int3 begin = Int3(0, 0, 0);
     Int3 end = grid->numCells;
@@ -33,18 +35,29 @@ void initializeGrid(std::unique_ptr<GridType>& grid)
     for (int i = begin.x; i < end.x; i++)
         for (int j = begin.y; j < end.y; j++)
             for (int k = begin.z; k < end.z; k++) {
-                grid->Ex(i, j, k) = eFunc(grid->ExPosition(i, j, k), 0).x;
-                grid->Ey(i, j, k) = eFunc(grid->EyPosition(i, j, k), 0).y;
-                grid->Ez(i, j, k) = eFunc(grid->EzPosition(i, j, k), 0).z;
+                grid->Ex(i, j, k) = eFunc(grid->ExPosition(i, j, k) + coordOffset, 0).x;
+                grid->Ey(i, j, k) = eFunc(grid->EyPosition(i, j, k) + coordOffset, 0).y;
+                grid->Ez(i, j, k) = eFunc(grid->EzPosition(i, j, k) + coordOffset, 0).z;
 
-                grid->Bx(i, j, k) = bFunc(grid->BxPosition(i, j, k), 0).x;
-                grid->By(i, j, k) = bFunc(grid->ByPosition(i, j, k), 0).y;
-                grid->Bz(i, j, k) = bFunc(grid->BzPosition(i, j, k), 0).z;
+                grid->Bx(i, j, k) = bFunc(grid->BxPosition(i, j, k) + coordOffset, 0).x;
+                grid->By(i, j, k) = bFunc(grid->ByPosition(i, j, k) + coordOffset, 0).y;
+                grid->Bz(i, j, k) = bFunc(grid->BzPosition(i, j, k) + coordOffset, 0).z;
             }
 }
 
-int main()
+}
+
+int main(int argc, char** argv)
 {
+    using FP3 = pfc::FP3;
+    using Int3 = pfc::Int3;
+
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     Int3 gridSize = Int3(8, 8, 8);
     FP3 minCoords = FP3(0, 0, 0);
     FP3 maxCoords = FP3(1, 1, 1);
@@ -53,11 +66,30 @@ int main()
     std::unique_ptr<GridType> grid;
     grid.reset(new GridType(gridSize, minCoords, gridStep, gridSize));
 
-    initializeGrid(grid);
+    FP3 coordOffset = FP3(0.0, 0.0, 0.0);
 
-    std::cout << std::endl << "Hello Grid!" << std::endl;
+    if (rank == 1) coordOffset += FP3(1.0, 0.0, 0.0);
+    else if (rank == 2) coordOffset += FP3(0.0, 1.0, 0.0);
+    else if (rank == 3) coordOffset += FP3(1.0, 1.0, 0.0);
 
+    pfc::initializeGrid(grid, coordOffset);
+
+    std::cout << std::endl << "Hello Grid! From rank:" << rank << " : " << grid->getB(pfc::FP3(0.5, 0.5, 0.5)) << std::endl;
+
+    MPI_Finalize();
     return 0;
 }
 
-}
+
+// int main(int argc, char** argv) {
+//     MPI_Init(&argc, &argv);
+
+//     int rank, size;
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+//     std::cout << "Hello Grid from rank " << rank << " of " << size << std::endl;
+    
+//     MPI_Finalize();
+//     return 0;
+// }
