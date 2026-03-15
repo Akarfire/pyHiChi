b47c61a79bdfc9db42c05a622c8c9ec15aa39047
K <Akarfire@gmail.com>
2026-03-16 01:00:20 +0300

    [DEV] Experimenting with field transfer over MPI

--- src/MPI/src/Main.cpp
+++ src/MPI/src/Main.cpp
@@ -1,5 +1,8 @@
 #include <mpi.h>
 
+#include <functional>
+#include <string>
+
 #include "Constants.h"
 #include "FieldSolver.h"
 #include "Grid.h"
@@ -9,49 +12,110 @@
 #include "Vectors.h"
 
 using GridType = pfc::YeeGrid;
-namespace pfc {
+using FP = pfc::FP;
+using FP3 = pfc::FP3;
+using Int3 = pfc::Int3;
 
- FP3 eFunc(FP3 coords, FP t) 
- {
-    FP3 e;
-    for (int i = 0; i < 3; i++)
-        e[i] = sin((coords.x + coords.y) * coords.z + i*i);
-    return e;
-}
+#define testFieldComponent Ey
 
-FP3 bFunc(FP3 coords, FP t) 
+FP3 eFunc_0(FP3 coords, FP t) 
 {
-    FP3 e;
-    for (int i = 0; i < 3; i++)
-        e[i] = sin((coords.x + coords.y) * coords.z + i*i);
-    return e;
+    return coords;
+    // FP3 e;
+    // for (int i = 0; i < 3; i++)
+    //     e[i] = sin((coords.x + coords.y) * coords.z + i*i);
+    // return e;
 }
 
-void initializeGrid(std::unique_ptr<GridType>& grid, const FP3& coordOffset) 
+FP3 bFunc_0(FP3 coords, FP t) 
+{
+    return coords;
+    // FP3 e;
+    // for (int i = 0; i < 3; i++)
+    //     e[i] = sin((coords.x + coords.y) * coords.z + i*i);
+    // return e;
+}
+
+FP3 eFunc_1(FP3 coords, FP t) 
+{
+    return coords + FP3(1, 1, 1); //FP3(0, 0, 0);
+    // FP3 e;
+    // for (int i = 0; i < 3; i++)
+    //     e[i] = sin((coords.x + coords.y) * coords.z + i*i);
+    // return e;
+}
+
+FP3 bFunc_1(FP3 coords, FP t) 
+{
+    return coords + FP3(1, 1, 1);
+    // FP3 e;
+    // for (int i = 0; i < 3; i++)
+    //     e[i] = sin((coords.x + coords.y) * coords.z + i*i);
+    // return e;
+}
+
+void initializeGrid(std::unique_ptr<GridType>& grid, int rank) 
 {
     Int3 begin = Int3(0, 0, 0);
     Int3 end = grid->numCells;
 
+    std::function<FP3(FP3, FP)> eFunc = eFunc_0;
+    std::function<FP3(FP3, FP)> bFunc = bFunc_0;
+
+    if (rank == 1)
+    {
+        eFunc = eFunc_1;
+        bFunc = bFunc_1;
+    }
+
     for (int i = begin.x; i < end.x; i++)
         for (int j = begin.y; j < end.y; j++)
             for (int k = begin.z; k < end.z; k++) {
-                grid->Ex(i, j, k) = eFunc(grid->ExPosition(i, j, k) + coordOffset, 0).x;
-                grid->Ey(i, j, k) = eFunc(grid->EyPosition(i, j, k) + coordOffset, 0).y;
-                grid->Ez(i, j, k) = eFunc(grid->EzPosition(i, j, k) + coordOffset, 0).z;
+                grid->Ex(i, j, k) = eFunc(grid->ExPosition(i, j, k), 0).x;
+                grid->Ey(i, j, k) = eFunc(grid->EyPosition(i, j, k), 0).y;
+                grid->Ez(i, j, k) = eFunc(grid->EzPosition(i, j, k), 0).z;
 
-                grid->Bx(i, j, k) = bFunc(grid->BxPosition(i, j, k) + coordOffset, 0).x;
-                grid->By(i, j, k) = bFunc(grid->ByPosition(i, j, k) + coordOffset, 0).y;
-                grid->Bz(i, j, k) = bFunc(grid->BzPosition(i, j, k) + coordOffset, 0).z;
+                grid->Bx(i, j, k) = bFunc(grid->BxPosition(i, j, k), 0).x;
+                grid->By(i, j, k) = bFunc(grid->ByPosition(i, j, k), 0).y;
+                grid->Bz(i, j, k) = bFunc(grid->BzPosition(i, j, k), 0).z;
             }
 }
 
+void debugPrintGrid_xyplane(std::unique_ptr<GridType>& grid, int z)
+{
+    Int3 size = grid->numCells;
+
+    for (int i = 0; i < size.x; i++)
+    {
+        std::string line = "";
+        for (int j = 0; j < size.y; j++)
+            line += std::to_string(grid->testFieldComponent(i, j, z)) + ", ";
+        std::cout << line << std::endl;
+    }
+
+    std::cout << std::endl << std::endl;
+}
+
+void define_line_type(MPI_Datatype &out_type, int count, int stride, int block_size = 1)
+{
+    MPI_Type_vector(count, block_size, stride, MPI_DOUBLE, &out_type);
+    MPI_Type_commit(&out_type);
+}
+
+void define_plane_type(MPI_Datatype &out_type, const MPI_Datatype &line_type, int count, int stride)
+{
+    // Calculating stride in bytes
+    MPI_Aint byte_stride;
+    MPI_Type_extent(MPI_DOUBLE, &byte_stride);
+    byte_stride *= stride;
+    
+    // Creating a vector type with a byte stride
+    MPI_Type_create_hvector(count, 1, byte_stride, line_type, &out_type);
+    MPI_Type_commit(&out_type);
 }
 
 int main(int argc, char** argv)
 {
-    using FP3 = pfc::FP3;
-    using Int3 = pfc::Int3;
-
     MPI_Init(&argc, &argv);
 
     int rank, size;
@@ -66,15 +130,82 @@ int main(int argc, char** argv)
     std::unique_ptr<GridType> grid;
     grid.reset(new GridType(gridSize, minCoords, gridStep, gridSize));
 
-    FP3 coordOffset = FP3(0.0, 0.0, 0.0);
+    initializeGrid(grid, rank);
 
-    if (rank == 1) coordOffset += FP3(1.0, 0.0, 0.0);
-    else if (rank == 2) coordOffset += FP3(0.0, 1.0, 0.0);
-    else if (rank == 3) coordOffset += FP3(1.0, 1.0, 0.0);
+    if (rank == 1)
+    {    
+        debugPrintGrid_xyplane(grid, 0);
+        debugPrintGrid_xyplane(grid, 1);
+        debugPrintGrid_xyplane(grid, 2);
+    }
 
-    pfc::initializeGrid(grid, coordOffset);
+    // MPI_Datatype X_lineType;
+    // define_line_type(X_lineType, grid->numCells.x, grid->numCells.y * grid->numCells.z, 1);
 
-    std::cout << std::endl << "Hello Grid! From rank:" << rank << " : " << grid->getB(pfc::FP3(0.5, 0.5, 0.5)) << std::endl;
+    // MPI_Datatype XZ_planeType;
+    // define_plane_type(XZ_planeType, X_lineType, grid->numCells.z, 1);
+
+    // if (rank == 0)
+    //     MPI_Send(grid->testFieldComponent.getData(), 1, XZ_planeType, 1, 0, MPI_COMM_WORLD);
+    // if (rank == 1)
+    // {
+    //     MPI_Status status;
+    //     MPI_Recv(grid->testFieldComponent.getData(), 1, XZ_planeType, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+    //     std::cout << "MPI SEND ---------------------------------------------- MPI RECV" << std::endl << std::endl << std::endl;
+
+    //     debugPrintGrid_xyplane(grid, 0);
+    //     debugPrintGrid_xyplane(grid, 1);
+    //     debugPrintGrid_xyplane(grid, 2);
+    // }
+
+    // MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Datatype Y_lineType;
+    define_line_type(Y_lineType, grid->numCells.y, grid->numCells.z, 1);
+
+    MPI_Datatype YZ_planeType;
+    define_plane_type(YZ_planeType, Y_lineType, grid->numCells.z, 1);
+
+    if (rank == 0)
+        MPI_Send(grid->testFieldComponent.getData(), 1, YZ_planeType, 1, 0, MPI_COMM_WORLD);
+    if (rank == 1)
+    {
+        MPI_Status status;
+        int offset = grid->numCells.x * grid->numCells.y * grid->numCells.z - grid->numCells.y * grid->numCells.z;
+        MPI_Recv(grid->testFieldComponent.getData() + offset, 1, YZ_planeType, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        std::cout << "MPI SEND ---------------------------------------------- MPI RECV" << std::endl << std::endl << std::endl;
+
+        debugPrintGrid_xyplane(grid, 0);
+        debugPrintGrid_xyplane(grid, 1);
+        debugPrintGrid_xyplane(grid, 2);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // MPI_Datatype XY_planeType;
+    // define_plane_type(XY_planeType, X_lineType, grid->numCells.y, grid->numCells.z);
+
+    // if (rank == 0)
+    //     MPI_Send(grid->testFieldComponent.getData(), 1, XY_planeType, 1, 0, MPI_COMM_WORLD);
+    // if (rank == 1)
+    // {
+    //     MPI_Status status;
+    //     MPI_Recv(grid->testFieldComponent.getData(), 1, XY_planeType, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+    //     std::cout << "MPI SEND ---------------------------------------------- MPI RECV" << std::endl << std::endl << std::endl;
+
+    //     debugPrintGrid_xyplane(grid, 0);
+    //     debugPrintGrid_xyplane(grid, 1);
+    //     debugPrintGrid_xyplane(grid, 2);
+    // }
+
+    // // Clean up
+    // MPI_Type_free(&X_lineType);
+    // MPI_Type_free(&XZ_planeType);
+    // MPI_Type_free(&Y_lineType);
+    // MPI_Type_free(&YZ_planeType);
 
     MPI_Finalize();
     return 0;
