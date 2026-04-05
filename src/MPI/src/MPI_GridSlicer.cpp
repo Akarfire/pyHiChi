503eb0fb23d3307a502cee7d06309a49ca029cb8
K <Akarfire@gmail.com>
2026-04-05 23:55:06 +0300

    [MPI DEV] Grid Slicer rewrite (now tested)

--- src/MPI/src/MPI_GridSlicer.cpp
+++ src/MPI/src/MPI_GridSlicer.cpp
@@ -16,27 +16,17 @@ void GridSlicer::getSubGridParameters(pfc::FP3& out_origin, pfc::Int3& out_size,
 
     Topology::LoopType loopType = topology.getLoopType();
 
-    pfc::Int3 l_border_overlap;
-    l_border_overlap.x = topology.doesLoopOverX(loopType) ? (-1) : 0;
-    l_border_overlap.y = topology.doesLoopOverY(loopType) ? (-1) : 0;
-    l_border_overlap.z = topology.doesLoopOverZ(loopType) ? (-1) : 0;
-
-    pfc::Int3 r_border_overlap;
-    r_border_overlap.x = topology.doesLoopOverX(loopType) ? (sections.x) : sections.x - 1;
-    r_border_overlap.y = topology.doesLoopOverY(loopType) ? (sections.y) : sections.y - 1;
-    r_border_overlap.z = topology.doesLoopOverZ(loopType) ? (sections.z) : sections.z - 1;
-
     pfc::Int3 lb;
     pfc::Int3 rb;
 
-    lb.x = ( (node.x - 1 < 0) ? l_border_overlap.x : divisions[0][node.x - 1] - 1 );
-    rb.x = ( (node.x == r_border_overlap.x) ? main_size.x : divisions[0][node.x] + 1 );
+    lb.x = (node.x - 1 < 0) ? 0 : (divisions[0][node.x - 1]);
+    rb.x = (node.x == sections.x - 1) ? main_size.x : (divisions[0][node.x]);
 
-    lb.y = ( (node.y - 1 < 0) ? l_border_overlap.y : divisions[1][node.y - 1] - 1 );
-    rb.y = ( (node.y == r_border_overlap.y) ? main_size.y : divisions[1][node.y] + 1 );
+    lb.y = (node.y - 1 < 0) ? 0 : (divisions[1][node.y - 1]);
+    rb.y = (node.y == sections.y - 1) ? main_size.y : (divisions[1][node.y]);
 
-    lb.z = ( (node.z - 1 < 0) ? l_border_overlap.z : divisions[2][node.z - 1] - 1 );
-    rb.z = ( (node.z == r_border_overlap.z) ? main_size.z : divisions[2][node.z] + 1 );
+    lb.z = (node.z - 1 < 0) ? 0 : (divisions[1][node.z - 1]);
+    rb.z = (node.z == sections.z - 1) ? main_size.z : (divisions[1][node.z]);
 
     // Outputs
     out_origin = main_origin + step * lb;
